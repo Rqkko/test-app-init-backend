8ee5d2384e720652992ee82fd2394095f6727143
Rqkko <rakko26@gmail.com>
2025-03-21 00:34:29 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
+# filepath: /Users/rakko/UserDocuments/Studies/University/Year3/Semester2/DES422_BusinessApplicationDevelopment/TestProject/backend/app/main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import os
 from supabase import create_client, Client
 
@@ -6,12 +8,25 @@ app = FastAPI()
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
-print(SUPABASE_KEY)
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+# Add CORS middleware
+origins = [
+    "http://localhost:3000",  # React development server
+    "https://test-app-init-frontend-production.up.railway.app/",  # Your deployed frontend URL
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
-    return {"message": "FastAPI Backend with Supabase " + SUPABASE_URL}
+    return {"message": "FastAPI Backend with Supabase"}
 
 @app.get("/users")
 def get_users():
